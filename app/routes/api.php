125533e38a97b23f5e3cc1d339238d5ad5d5fdec
TalysonSoares <talysonsoares40@gmail.com>
2024-04-08 15:32:34 -0300

    Create API for agents

--- app/routes/api.php
+++ app/routes/api.php
@@ -2,12 +2,15 @@
 
 declare(strict_types=1);
 
+use App\Controller\Api\AgentApiController;
 use App\Controller\Api\SealApiController;
 use App\Controller\Api\WelcomeApiController;
 
 return [
     '/api' => [WelcomeApiController::class, 'index'],
     '/api/v2' => [WelcomeApiController::class, 'index'],
+    '/api/v2/agents' => [AgentApiController::class, 'getList'],
+    '/api/v2/agents/{id}' => [AgentApiController::class, 'getOne'],
     '/api/v2/seals' => [SealApiController::class, 'getList'],
     '/api/v2/seals/{id}' => [SealApiController::class, 'getOne'],
 ];
