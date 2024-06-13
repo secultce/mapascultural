31dc8ee4628260c2e47dfc48ae7053659afc90ea
Ana Kelly Moura Balbino <101601082+Akellymourab@users.noreply.github.com>
2024-06-13 09:43:01 -0300

    Update cypress dependencies, create v2 documentation test folder and some configurations in cypress folders. (#300)

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
     e2e: {
-        baseUrl: 'https://redesign.testes.map.as',
+        baseUrl: 'http://localhost',
         chromeWebSecurity: false,
         setupNodeEvents(on, config) {
         },
