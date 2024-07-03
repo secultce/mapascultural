dd3b35546d859a834c80298d3bd75ef0a52c5fc5
TalysonSoares <talysonsoares40@gmail.com>
2024-07-03 15:42:39 -0300

    Improvement spaces end-to-end tests

--- cypress/e2e/spacesPage/compact.cy.js
+++ cypress/e2e/spacesPage/compact.cy.js
@@ -1,25 +1,54 @@
-describe("Homepage compactada", () => {
-    beforeEach(() => {
-        cy.viewport(1000, 768);
-        cy.visit("/");
-    });
-
-    it("acessa \"Espaços\" no navbar", () => {
-        cy.get(".mc-header-menu__btn-mobile").click();
-        cy.contains(".mc-header-menu__itens a", "Espaços").click();
-        cy.url().should("include", "/espacos/#list");
-    });
-});
-
+const { searchBar } = require("../../commands/searchBar");
+const { clearAllFilters } = require("../../commands/clearAllFilters");
 describe("Pagina de Espaços", () => {
-    beforeEach(() => {
-        cy.viewport(1000, 768);
-        cy.visit("/espacos/#list");
-    });
+  beforeEach(() => {
+    cy.viewport(500, 768);
+    cy.visit("/espacos/#list");
+  });
 
-    it("clica em \"Acessar\" e entra na pagina no espaço selecionado", () => {
-        cy.get(":nth-child(2) > .entity-card__footer > .entity-card__footer--action > .button").click();
-        cy.url().should("include", "/espaco/34/#info");
-        cy.contains("Henri Hyatt");
+  it("Garante que o usuário possa ver todos os espaços disponíveis", () => {
+    const texts = [
+      "3 Espaços encontrados",
+      "Secretaria da Cultura do Estado do Ceará - SECULT",
+      "Biblioteca Municipal Pedro Maia Rocha",
+      "Centro Cultural Carnaubeira"
+    ];
+
+    texts.forEach(text => {
+      cy.contains(text);
     });
+  });
+
+  it("Clica em \"Acessar\" e entra na pagina no espaço selecionado", () => {
+    cy.get(":nth-child(2) > .entity-card__footer > .entity-card__footer--action > .button").click();
+    cy.url().should("include", "/espaco/1/#info");
+    cy.contains("Secretaria da Cultura do Estado do Ceará - SECULT");
+  });
+
+  it("Garante que os filtros na página de espaços funcionam", () => {
+    cy.get(".search-filter__actions > .search-filter__actions--formBtn").click();
+    cy.get(":nth-child(2) > .mc-multiselect > :nth-child(1) > .mc-multiselect--input").click();
+    cy.get(":nth-child(18) > .item > .text").click({ multiple: true, force: true });
+    cy.wait(1000);
+    cy.contains("1 Espaços encontrado");
+    cy.contains("Biblioteca Municipal Pedro Maia Rocha");
+  });
+
+  it("Garante que o usuário consiga usar o campo de pesquisa", () => {
+    searchBar("Carnaubeira", "1 Espaços encontrados", "Centro Cultural Carnaubeira");
+  });
+
+  it("Garante que o botão limpar filtros na página de espaços funcionam", () => {
+    clearAllFilters([
+      ".search-filter__actions > .search-filter__actions--formBtn",
+      ".form > :nth-child(1) > :nth-child(2)",
+      ".verified",
+      ":nth-child(2) > .mc-multiselect > :nth-child(1) > .mc-multiselect--input",
+      ":nth-child(1) > .item > .text",
+      ".modal__close",
+      ":nth-child(3) > .mc-multiselect > div > .mc-multiselect--input",
+      ":nth-child(1) > .item > .text",
+      ".modal__close"
+    ], "3 Espaços encontrados");
+  });
 });
--- cypress/e2e/spacesPage/full.cy.js
+++ cypress/e2e/spacesPage/full.cy.js
@@ -1,25 +1,51 @@
 const { clearAllFilters } = require("../../commands/clearAllFilters");
+const { searchBar } = require("../../commands/searchBar");
 
 describe("Pagina de Espaços", () => {
-    beforeEach(() => {
-        cy.viewport(1920, 1080);
-        cy.visit("/espacos/#list");
-    });
+  beforeEach(() => {
+    cy.viewport(1920, 1080);
+    cy.visit("/espacos/#list");
+  });
 
-    it("clica em \"Acessar\" e entra na pagina no espaço selecionado", () => {
-        cy.get(":nth-child(2) > .entity-card__footer > .entity-card__footer--action > .button").click();
-        cy.url().should("include", "/espaco/34/#info");
-        cy.contains("Henri Hyatt");
-    });
+  it("Garante que o usuário possa ver todos os espaços disponíveis", () => {
+    const texts = [
+      "3 Espaços encontrados",
+      "Secretaria da Cultura do Estado do Ceará - SECULT",
+      "Biblioteca Municipal Pedro Maia Rocha",
+      "Centro Cultural Carnaubeira"
+    ];
 
-    it("Garante que o botão limpar filtros na pagina de espaços funciona", () => {
-        clearAllFilters([
-            ".form > :nth-child(1) > :nth-child(2)",
-            ".verified",
-            ":nth-child(2) > .mc-multiselect > :nth-child(1) > .v-popper > .mc-multiselect--input",
-            ":nth-child(1) > .item > .text",
-            ":nth-child(3) > .mc-multiselect > :nth-child(1) > .v-popper > .mc-multiselect--input",
-            ":nth-child(1) > .item > .text"
-        ], "30 Espaços encontrados");
+    texts.forEach(text => {
+      cy.contains(text);
     });
+  });
+
+  it("Clica em \"Acessar\" e entra na pagina no espaço selecionado", () => {
+    cy.get(":nth-child(2) > .entity-card__footer > .entity-card__footer--action > .button").click();
+    cy.url().should("include", "/espaco/1/#info");
+    cy.contains("Secretaria da Cultura do Estado do Ceará - SECULT");
+  });
+
+  it("Garante que os filtros na página de espaços funcionam", () => {
+    cy.get(":nth-child(2) > .mc-multiselect > :nth-child(1) > .v-popper > .mc-multiselect--input").click();
+    cy.get(":nth-child(18) > .item > .text").click({ multiple: true, force: true });
+    cy.wait(1000);
+    cy.contains("1 Espaços encontrado");
+    cy.contains("Biblioteca Municipal Pedro Maia Rocha");
+  });
+
+  it("Garante que o usuário consiga usar o campo de pesquisa", () => {
+    searchBar("Carnaubeira", "1 Espaços encontrados", "Centro Cultural Carnaubeira");
+  });
+
+  it("Garante que o botão limpar filtros na página de espaços funcionam", () => {
+    clearAllFilters([
+      ".form > :nth-child(1) > :nth-child(2)",
+      ".verified",
+      ":nth-child(2) > .mc-multiselect > :nth-child(1) > .v-popper > .mc-multiselect--input",
+      ":nth-child(1) > .item > .text",
+      ":nth-child(3) > .mc-multiselect > :nth-child(1) > .v-popper > .mc-multiselect--input",
+      ":nth-child(1) > .item > .text"
+    ], "3 Espaços encontrados");
+  });
 });
