51d5422c01a187ae508a6eebc3f479210911094f
Talyson Soares <talysonsoares40@gmail.com>
2024-07-04 10:08:34 -0300

    Improvement spaces end-to-end tests (#329)

---
+++ cypress/commands/searchBar.js
@@ -0,0 +1,13 @@
+function searchBar (searchText, expectedResultsCount, expectedText) {
+    cy.get(".search-filter__actions--form > .search-filter__actions--form-input").type(searchText);
+
+    if (expectedResultsCount) {
+        cy.contains(expectedResultsCount);
+    }
+
+    if (expectedText) {
+        cy.contains(expectedText);
+    }
+}
+
+module.exports = { searchBar };
