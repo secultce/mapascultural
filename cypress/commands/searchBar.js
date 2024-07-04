04b44080acf4c1b2927545367aa8b06205eadaae
Ana Kelly Moura Balbino <101601082+Akellymourab@users.noreply.github.com>
2024-07-04 18:42:02 -0300

    Adjusts tests to match eslint rules and best practices. (#342)

--- cypress/commands/searchBar.js
+++ cypress/commands/searchBar.js
@@ -1,13 +1,13 @@
 function searchBar (searchText, expectedResultsCount, expectedText) {
-    cy.get(".search-filter__actions--form > .search-filter__actions--form-input").type(searchText);
+  cy.get(".search-filter__actions--form > .search-filter__actions--form-input").type(searchText);
 
-    if (expectedResultsCount) {
-        cy.contains(expectedResultsCount);
-    }
+  if (expectedResultsCount) {
+    cy.contains(expectedResultsCount);
+  }
 
-    if (expectedText) {
-        cy.contains(expectedText);
-    }
+  if (expectedText) {
+    cy.contains(expectedText);
+  }
 }
 
 module.exports = { searchBar };
