92d924f82d52c53d3d634620fcafeb2c34449200
Sara Camilo <saraccamilo@gmail.com>
2024-07-04 18:33:30 -0300

    Adjusting footer tests

--- cypress/commands/login.js
+++ cypress/commands/login.js
@@ -1,23 +1,6 @@
-const { confirmRecaptcha } = require("./recaptcha");
-
 function login () {
   cy.visit("/autenticacao/");
-  cy.get("input[id='email']").type("Admin@local");
-  cy.get("input[id='password']").type("mapas123");
-  confirmRecaptcha();
-  cy.wait(1000);
-  cy.get("button[type='submit']").click();
-  cy.url().should("include", "painel/#main");
+  cy.get("a[href=\"http://localhost/autenticacao/fakeLogin/?fake_authentication_user_id=1\"]").contains("Fazer login com este usuário").click();
 }
 
-function loginWith (username, password) {
-  cy.get("input[id='email']").type(username);
-  if (password) {
-    cy.get("input[id='password']").type(password);
-  }
-  confirmRecaptcha();
-  cy.wait(1000);
-  cy.get("button[type='submit']").click();
-}
-
-module.exports = { login, loginWith };
+module.exports = { login };
--- cypress/e2e/footer/index.cy.js
+++ cypress/e2e/footer/index.cy.js
@@ -3,6 +3,7 @@ import { footer } from "../../commands/footer";
 describe("Homepage", () => {
   beforeEach(() => {
     cy.visit("/");
+    cy.log("Página inicial carregada");
   });
 
   it("testa a funcão footer", () => {
