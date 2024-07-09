5d52994fac9f8b0afddb6cc2b043a10729026bcb
TalysonSoares <talysonsoares40@gmail.com>
2024-07-09 14:22:10 -0300

    Improvement agents end-to-end tests

--- cypress/commands/searchBar.js
+++ cypress/commands/searchBar.js
@@ -2,10 +2,12 @@ function searchBar (searchText, expectedResultsCount, expectedText) {
   cy.get(".search-filter__actions--form > .search-filter__actions--form-input").type(searchText);
 
   if (expectedResultsCount) {
+    cy.wait(1200);
     cy.contains(expectedResultsCount);
   }
 
   if (expectedText) {
+    cy.wait(1200);
     cy.contains(expectedText);
   }
 }
--- cypress/e2e/agentesPage/compact.cy.js
+++ cypress/e2e/agentesPage/compact.cy.js
@@ -1,23 +1,56 @@
-describe("Homepage compactada", () => {
-  beforeEach(() => {
-    cy.viewport(1000, 768);
-    cy.visit("/");
-  });
-  it("acessa \"Agentes\" no navbar", () => {
-    cy.get(".mc-header-menu__btn-mobile").click();
-    cy.contains(".mc-header-menu__itens a", "Agentes").click();
-    cy.url().should("include", "agentes");
-  });
-});
+const { clearAllFilters } = require("../../commands/clearAllFilters");
+const { searchBar } = require("../../commands/searchBar");
+
 describe("Pagina de Agentes", () => {
   beforeEach(() => {
-    cy.viewport(1000, 768);
+    cy.viewport(500, 768);
     cy.visit("/agentes");
   });
 
-  it("clica em \"Acessar\" e entra na pagina no agente selecionado", () => {
-    cy.get(":nth-child(2) > .entity-card__footer > .entity-card__footer--action > .button").click();
-    cy.url().should("include", "/agente/118/#info");
-    cy.contains("Junin Oliveira");
+  it("Garante que o usuário possa ver os agentes existentes", () => {
+    const texts = [
+      "6 Agentes encontrados",
+      "Admin@local",
+      "Alessandro Feitoza",
+      "Henrique Lima",
+      "Anna Kelly Moura",
+      "Sara Camilo",
+      "Talyson Soares"
+    ];
+
+    texts.forEach(text => {
+      cy.contains(text);
+    });
+  });
+
+  it("Garante que os filtros na página de agentes funcionam", () => {
+    cy.get(".search-filter__actions--formBtn").click({ force: true });
+    cy.get(":nth-child(2) > select").select("Agente Individual");
+    cy.get(":nth-child(2) > select").should("have.value", "1");
+    cy.get(".search-filter__filter--close").click();
+
+    cy.contains("1 Agentes encontrados");
+    cy.contains("Admin@local");
+  });
+
+  it("Garante que o botão limpar filtros da página de agentes funcionam", () => {
+    clearAllFilters([
+      ".search-filter__actions--formBtn",
+      ".verified > input",
+      ":nth-child(2) > select",
+      ".mc-multiselect--input",
+      ":nth-child(1) > .item > .text",
+      ".modal__close"
+    ], "6 Agentes encontrados");
+  });
+
+  it("Garante que o usuário consiga usar o campo de pesquisa", () => {
+    searchBar("Talyson", "1 Agentes encontrados", "Talyson Soares");
+  });
+
+  it("clica em \"Acessar\" e entra na pagina do agente selecionado", () => {
+    cy.get(":nth-child(7) > .entity-card__footer > .entity-card__footer--action > .button").click();
+    cy.url().should("include", "agente/6/#info");
+    cy.contains("Talyson Soares");
   });
 });
